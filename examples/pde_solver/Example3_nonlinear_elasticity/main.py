79ad9974471fe770e2972d2c202aa578cca1b1c6
zhangxx2643 <zhangxx2643@gmail.com>
2022-03-04 23:27:08 -0500

    update pde workflow example2 and 3

--- examples/pde_solver/Example3_nonlinear_elasticity/main.py
+++ examples/pde_solver/Example3_nonlinear_elasticity/main.py
@@ -1,11 +1,5 @@
-from mechanoChemML.workflows.physics_constrained_learning.src.pde_system_elasticity_nonlinear import WeakPDENonLinearElasticity as thisPDESystem
+from mechanoChemML.workflows.pde_solver.pde_system_elasticity_nonlinear import WeakPDENonLinearElasticity as thisPDESystem
 
 if __name__ == '__main__':
     problem = thisPDESystem()
     problem.run()
-    problem.test(test_folder='DNS')
-    problem.test(test_folder='Inter')
-    problem.test(test_folder='Extra')
-    # problem.debug_problem(use_label=False)
-    # problem.debug_problem(use_label=True)
-    # problem.test_residual_gaussian(noise_std=1e-4, sample_num=1000)
