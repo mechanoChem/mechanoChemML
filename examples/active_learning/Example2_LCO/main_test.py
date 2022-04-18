bdbd1375c3edd0fb63896371ca780d27d1b419ff
Xiaoxuan Zhang <zhangxx2643@gmail.com>
2022-04-18 17:30:39 -0400

    re-organize active learning workflow

--- examples/active_learning/Example2_LCO/main_test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import os
-from active_learningLCO import Active_learning
-
-workflow = Active_learning(os.path.dirname(__file__)+'/LCO_test.ini',test=True)
-workflow.main_workflow()
