82673684148abb5daa8c06b10b32633f31f06afa
Mostafa-sh <mfshojaei@gmail.com>
2022-04-21 17:24:00 -0400

    Update main_test.py

--- examples/active_learning/Example1_NiAl/main_test.py
+++ examples/active_learning/Example1_NiAl/main_test.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 import os
+import tensorflow as tf
 from mechanoChemML.workflows.active_learning.active_learning import Active_learning
+
+physical_devices = tf.config.list_physical_devices('GPU')
+try:
+  tf.config.experimental.set_memory_growth(physical_devices[0], True)
+except:
+  # Invalid device or cannot modify virtual devices once initialized.
+  pass
 
 input_path = os.path.dirname(__file__)
 if input_path: # when it is not ''
